72b9f0ea10fe28deecb7734d5e6c53767601dd12
Aaron Avila <aaronvendedor@gmail.com>
2026-01-22 08:52:24 -0400

    feat: added code block rendering

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 m2p is a cli tool made to generate fast pdfs from markdown files is so freaking fast, i dont understand why a tool like this is sooo fast
 
-inline code `render`
+inline code *`render`*
+
+```rust
+fn main() {
+    println!("Hello world!")
+}
+```
--- src/native.rs
+++ src/native.rs
@@ -1,8 +1,9 @@
-use crate::{builder::PDFBuilder, fonts::Fonts, metadata::Metadata};
+use crate::{builder::PDFBuilder, fonts::Fonts, metadata::Metadata, native::code::CodeBlock};
 use anyhow::Result;
 use genpdf::{
-    elements::{Break, Paragraph},
+    elements::{Break, CellDecorator, FrameCellDecorator, PaddedElement, Paragraph},
     fonts::FontData,
+    render::TextSection,
     style::{Style, StyledString},
     *,
 };
@@ -44,6 +45,7 @@ impl PDFBuilder for NativePDFBuilder {
         let mut style_stack = vec![Style::new()];
         let mut text_buffer: Vec<StyledString> = Vec::with_capacity(32);
         let mut current_block_style = Style::new();
+        let mut lang: Option<String> = None;
 
         for event in events {
             match event {
@@ -68,20 +70,33 @@ impl PDFBuilder for NativePDFBuilder {
                             style_stack.push(style_stack.last().unwrap().with_font_family(mono));
                         }
                         pulldown_cmark::CodeBlockKind::Fenced(code) => {
-                            todo!("fenced")
+                            lang = Some(code.into());
+
+                            doc.push(Break::new(1));
                         }
                     },
                     Tag::Strong => style_stack.push(style_stack.last().unwrap().bold()),
                     Tag::Emphasis => style_stack.push(style_stack.last().unwrap().italic()),
                     _ => {}
                 },
-
                 Event::Text(text) => {
+                    if let Some(code) = lang {
+                        let block = CodeBlock::new(&text, mono, code);
+
+                        doc.push(block);
+
+                        lang = None;
+                        continue;
+                    }
                     if let Some(style) = style_stack.last() {
                         text_buffer.push(StyledString::new(text, *style));
                     }
                 }
-
+                Event::Code(text) => {
+                    if let Some(style) = style_stack.last() {
+                        text_buffer.push(StyledString::new(text, style.with_font_family(mono)));
+                    }
+                }
                 Event::End(tag) => match tag {
                     TagEnd::Paragraph | TagEnd::Heading(_) => {
                         let mut p = Paragraph::new("");
@@ -111,5 +126,123 @@ impl PDFBuilder for NativePDFBuilder {
 
         let result = w.into_inner().map_err(|e| anyhow::anyhow!(e))?;
         Ok(result)
+    }
+}
+
+mod code {
+    use std::{
+        rc::Rc,
+        sync::{Arc, Mutex},
+    };
+
+    use genpdf::{
+        Element, Margins, Mm, Position, RenderResult,
+        elements::{Break, Paragraph},
+        fonts::{Font, FontFamily},
+        render::TextSection,
+        style::{Color, Style, StyledString},
+    };
+    use syntect::{
+        easy::HighlightLines,
+        highlighting::{Style as SyntectStyle, ThemeSet},
+        parsing::SyntaxSet,
+        util::{LinesWithEndings, as_24_bit_terminal_escaped},
+    };
+
+    #[derive(Debug, Clone)]
+    pub struct CodeBlock {
+        text: String,
+        font_family: FontFamily<Font>,
+        lang: String,
+    }
+
+    impl CodeBlock {
+        pub fn new(text: &str, font_family: FontFamily<Font>, lang: String) -> Self {
+            CodeBlock {
+                text: text.to_owned(),
+                font_family,
+                lang,
+            }
+        }
+    }
+
+    impl Element for CodeBlock {
+        fn render(
+            &mut self,
+            context: &genpdf::Context,
+            mut area: genpdf::render::Area<'_>,
+            style: genpdf::style::Style,
+        ) -> Result<genpdf::RenderResult, genpdf::error::Error> {
+            let area = Arc::new(area);
+            let mut result = RenderResult::default();
+
+            let margins = Margins::all(1.0);
+
+            let b_result = Break::new(1).render(context, Arc::clone(&area).into(), style)?;
+
+            // TODO: add a function to simplify this syntax
+            result.size.width += b_result.size.width;
+            result.size.height += b_result.size.height;
+
+            area.add_margins(margins);
+
+            let ps = SyntaxSet::load_defaults_newlines();
+            let ts = ThemeSet::load_defaults();
+
+            let syntax = ps.find_syntax_by_token(&self.lang).unwrap();
+            let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.light"]);
+            let mut buffers: Vec<Vec<StyledString>> = Vec::new();
+
+            for line in LinesWithEndings::from(&self.text) {
+                let ranges: Vec<(SyntectStyle, &str)> = h.highlight_line(line, &ps).unwrap();
+                let mut line_buf: Vec<StyledString> = Vec::new();
+
+                for range in ranges.iter() {
+                    line_buf.push(StyledString::new(
+                        range.1,
+                        Style::new()
+                            .with_color(Color::Rgb(
+                                range.0.foreground.r,
+                                range.0.foreground.g,
+                                range.0.foreground.b,
+                            ))
+                            .with_font_family(self.font_family),
+                    ));
+                }
+
+                buffers.push(line_buf);
+            }
+
+            for mut buf in buffers {
+                let mut p = Paragraph::new("");
+
+                for span in buf.drain(..) {
+                    p.push(span);
+                }
+
+                let p_result = p.render(context, area.clone(), style)?;
+
+                result.size.width += p_result.size.width;
+
+                result.size.height += p_result.size.height;
+
+                let b_result = Break::new(1).render(context, area.clone(), style)?;
+
+                // TODO: add a function to simplify this syntax
+                result.size.width += b_result.size.width;
+                result.size.height += b_result.size.height;
+            }
+
+            let b_result = Break::new(1).render(context, area.clone(), style)?;
+
+            // TODO: add a function to simplify this syntax
+            result.size.width += b_result.size.width;
+            result.size.height += b_result.size.height;
+
+            result.size.width += 2.0.into();
+            result.size.height += 2.0.into();
+
+            Ok(result)
+        }
     }
 }
