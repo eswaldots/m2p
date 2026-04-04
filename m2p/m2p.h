24bad36173810d15777334fee5d556bea8e08cc4
Aaron Avila <aaronvendedor@gmail.com>
2026-04-04 18:39:51 -0400

    feat: added log level declarations on header files

--- m2p/m2p.h
+++ m2p/m2p.h
@@ -1,9 +1,16 @@
 typedef enum HEADING_FONT_SIZE {
-  HEADING_H1_SIZE = 20,
-  HEADING_H2_SIZE = 18,
-  HEADING_H3_SIZE = 16,
-  HEADING_H4_SIZE = 14,
-  HEADING_H5_SIZE = 12,
-  HEADING_H6_SIZE = 11,
-  P_SIZE = 12,
+        HEADING_H1_SIZE = 20,
+        HEADING_H2_SIZE = 18,
+        HEADING_H3_SIZE = 16,
+        HEADING_H4_SIZE = 14,
+        HEADING_H5_SIZE = 12,
+        HEADING_H6_SIZE = 11,
+        P_SIZE = 12,
 } HEADING_FONT_SIZE;
+
+typedef enum mp_log_level_t {
+        MP_LOG_ERROR,
+        MP_LOG_WARNING,
+        MP_LOG_DEBUG,
+        MP_LOG_INFO,
+} mp_log_level;
--- m2p/util.h
+++ m2p/util.h
@@ -1,1 +1,5 @@
+#include "m2p.h"
+
 char *change_ext(char *filename);
+
+char m2p_printf(mp_log_level_t level, const char *format, ...);
